3a633357589080e02fce3dfd0ac50a7bd651f88d
Shawn Liu <shawn.jw.liu@gmail.com>
2019-10-20 15:28:02 +0800

    fix checker Dockerfile issue

--- build-checker.sh
+++ build-checker.sh
@@ -3,7 +3,7 @@ rm checker/package.json
 cp package.json ./checker
 cd ./checker
 docker build -t registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval-checker .
-docker tag registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval-checker:latest registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval-checker:2.0
-docker push registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval-checker:2.0
+docker tag registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval-checker:latest registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval-checker:2.2
+docker push registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval-checker:2.2
 
 #docker run -it registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval-checker sh

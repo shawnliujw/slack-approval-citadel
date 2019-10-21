05f85f4cf374159aadff976486cdf6bb17622bd4
Shawn Liu <shawn.jw.liu@gmail.com>
2019-10-21 21:53:59 +0800

    change name

--- build-approval.sh
+++ build-approval.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build -t registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval .
-docker push registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval
+docker build -t registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval-citadel .
+docker push registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval-citadel
 #docker run -it --publish 7001:7001 -e REDIS_HOST=xxx -e SLACK_TOKEN=xxx -e SLACK_SECRET=xx -e SLACK_CHANNEL=xxx   registry.cn-shanghai.aliyuncs.com/shawn_repo/slack-approval
